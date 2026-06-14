64532cc10c974f71014b0cae823eada941351f74
Prince Sharma <princesharma17@gmail.com>
2026-06-14 18:17:02 +0530

    Add arithmetic operators example

---
+++ 01-basics/operators.c
@@ -0,0 +1,18 @@
+// Header file for input and output functions
+#include <stdio.h>
+
+// Main function
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    // Arithmetic operators
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+    printf("Division = %d\n", a / b);
+
+    // Program executed successfully
+    return 0;
+}
