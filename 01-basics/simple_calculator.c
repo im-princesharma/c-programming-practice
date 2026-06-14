b53b22a90b946e2fa886a621345ad7b458f8192d
Prince Sharma <princesharma17@gmail.com>
2026-06-15 00:14:31 +0530

    Add simple calculator program with beginner-friendly comments

---
+++ 01-basics/simple_calculator.c
@@ -0,0 +1,22 @@
+// Header file for input and output functions
+#include <stdio.h>
+
+// Main function
+int main()
+{
+    // Variable declaration
+    int num1, num2;
+
+    // Take input from user
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Perform arithmetic operations
+    printf("Addition = %d\n", num1 + num2);
+    printf("Subtraction = %d\n", num1 - num2);
+    printf("Multiplication = %d\n", num1 * num2);
+    printf("Division = %d\n", num1 / num2);
+
+    // Return 0 indicates successful execution
+    return 0;
+}
