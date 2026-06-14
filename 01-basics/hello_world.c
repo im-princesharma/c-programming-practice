2579151871c4f82af5e6a10d27446cebdccc4752
Prince Sharma <princesharma17@gmail.com>
2026-06-14 18:08:37 +0530

    Add Hello World program with beginner-friendly comments

---
+++ 01-basics/hello_world.c
@@ -0,0 +1,12 @@
+// Header file for input and output functions
+#include <stdio.h>
+
+// Main function - execution starts from here
+int main()
+{
+    // Print text on the screen
+    printf("Hello World\n");
+
+    // Return 0 means program executed successfully
+    return 0;
+}
