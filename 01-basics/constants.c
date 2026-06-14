5e7860f2d72cf103f51865061c82b729b2dd3db2
Prince Sharma <princesharma17@gmail.com>
2026-06-15 00:09:37 +0530

    Add constants example with beginner-friendly comments

---
+++ 01-basics/constants.c
@@ -0,0 +1,15 @@
+// Header file for input and output functions
+#include <stdio.h>
+
+// Main function
+int main()
+{
+    // Constant variable declaration
+    const float PI = 3.14;
+
+    // Display constant value
+    printf("Value of PI = %.2f\n", PI);
+
+    // Return 0 indicates successful execution
+    return 0;
+}
