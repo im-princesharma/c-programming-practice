e9a4a4b9f9967caa78948df9b908b077e2409d63
Prince Sharma <princesharma17@gmail.com>
2026-06-14 18:15:48 +0530

    Add variables and data types example

---
+++ 01-basics/variables.c
@@ -0,0 +1,15 @@
+// Header file for input and output functions
+#include <stdio.h>
+
+// Main function
+int main()
+{
+    // Variable declaration and initialization
+    int age = 19;
+
+    // Display variable value
+    printf("Age = %d\n", age);
+
+    // Program executed successfully
+    return 0;
+}
