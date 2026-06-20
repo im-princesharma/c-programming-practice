338901f5e987a2d8fb54e8a037eb3cc7983b1658
Prince Sharma <iprincesharma17@gmail.com>
2026-06-21 00:49:18 +0530

    Add function without parameter example demonstrating code modularity

---
+++ 04-functions/function_without_parameter.c
@@ -0,0 +1,32 @@
+/*
+==================================================
+Title   : Function Without Parameters
+
+Purpose : Demonstrate a user-defined function
+          without arguments.
+
+Working : displayMessage() prints predefined
+          messages and is called from main().
+
+==================================================
+*/
+
+#include <stdio.h>
+
+void displayMessage()
+{
+    printf("Welcome to C Programming\n");
+    printf("Functions help organize code\n");
+    printf("Learning functions is important\n");
+}
+
+int main()
+{
+    printf("Program Started\n\n");
+
+    displayMessage();
+
+    printf("\nProgram Ended\n");
+
+    return 0;
+}
