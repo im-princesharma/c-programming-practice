a5e549232c75d71f40712d009f39cccb41e4ea2c
Prince Sharma <iprincesharma17@gmail.com>
2026-07-28 19:18:18 +0530

    Update largest_element.c

--- 05-arrays/largest_element.c
+++ 05-arrays/largest_element.c
@@ -1,5 +1,5 @@
 /*
-==================================================        
+==================================================         
 Title   : Largest Element in Array
 Purpose : Find the largest value in an array.
 Working : Each element is compared with the 
